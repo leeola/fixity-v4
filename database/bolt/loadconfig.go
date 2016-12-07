e11490c31b83fb82330d3642d8acd074ef5791f3
Lee Olayvar <leeolayvar@gmail.com>
2016-12-06 18:24:10 -0800

    feat: support ~ in simple and bolt configs

--- database/bolt/loadconfig.go
+++ database/bolt/loadconfig.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/leeola/errors"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 func LoadConfig(configPath string) (Config, error) {
@@ -16,10 +17,19 @@ func LoadConfig(configPath string) (Config, error) {
 
 	var conf struct {
 		CreateMissingPaths bool
+		DontExpandHome     bool
 		BoltDatabase       Config
 	}
 	if _, err := toml.DecodeReader(f, &conf); err != nil {
 		return Config{}, errors.Wrap(err, "failed to unmarshal config")
+	}
+
+	if !conf.DontExpandHome && conf.BoltDatabase.BoltPath != "" {
+		p, err := homedir.Expand(conf.BoltDatabase.BoltPath)
+		if err != nil {
+			return Config{}, errors.Stack(err)
+		}
+		conf.BoltDatabase.BoltPath = p
 	}
 
 	// Create the db path if it's missing.
--- store/simple/loadconfig.go
+++ store/simple/loadconfig.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/leeola/errors"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 func LoadConfig(configPath string) (Config, error) {
@@ -16,10 +17,19 @@ func LoadConfig(configPath string) (Config, error) {
 
 	var conf struct {
 		CreateMissingPaths bool
+		DontExpandHome     bool
 		SimpleStore        Config `toml:"simpleStore"`
 	}
 	if _, err := toml.DecodeReader(f, &conf); err != nil {
 		return Config{}, errors.Wrap(err, "failed to unmarshal config")
+	}
+
+	if !conf.DontExpandHome && conf.SimpleStore.Path != "" {
+		p, err := homedir.Expand(conf.SimpleStore.Path)
+		if err != nil {
+			return Config{}, errors.Stack(err)
+		}
+		conf.SimpleStore.Path = p
 	}
 
 	// Create the db path if it's missing.
