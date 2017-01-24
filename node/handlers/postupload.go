2805222ae38fde1d5aa78cec37d56118b068eba7
Lee Olayvar <leeolayvar@gmail.com>
2017-01-24 06:52:48 -0800

    feat(index/bleve): implemented basic version index

--- node/handlers/postupload.go
+++ node/handlers/postupload.go
@@ -11,6 +11,8 @@ import (
 )
 
 func PostUploadHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	log := nodeware.GetLog(r)
 	changes := contenttype.NewChangesFromValues(r.URL.Query())
 
