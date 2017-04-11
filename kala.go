d329cb620cc07e9e90d36480b2f23fc534e2b8b7
Lee Olayvar <leeolayvar@gmail.com>
2017-04-11 07:31:50 -0700

    feat: fleshing out kala.Write method

--- kala.go
+++ kala.go
@@ -57,9 +57,47 @@ func (k *Kala) Write(c Commit, m Meta, r io.Reader) ([]string, error) {
 		return nil, errors.New("No data given to write")
 	}
 
-	// k.store.Write(
+	metaHash, err := store.MarshalAndWrite(k.store, store.Meta(m))
+	if err != nil {
+		return nil, errors.Stack(err)
+	}
 
-	return nil, nil
+	var multiBlobHash string
+	// TODO(leeola): Make this into a multipart splitter
+	// multiBlobHash, err := store.WriteReader(k.store, r)
+	// if err != nil {
+	// return nil, errors.Stack(err)
+	// }
+
+	version := store.Version{
+		MetaHash:      metaHash,
+		MultiBlobHash: multiBlobHash,
+	}
+
+	// TODO(leeola): load the old version if previous version hash is specified
+	// if c.PreviousVersionHash != "" {
+	// // .. load previous hash
+	// version = previousVersion
+	// }
+
+	versionHash, err := store.WriteVersion(s, version)
+	if err != nil {
+		return nil, errors.Stack(err)
+	}
+
+	// TODO(leeola): Index the metadata now that all has been written to the store.
+
+	// Replace the old changelog no matter what. Eg, even if we loaded an old version,
+	// the old version's changelog doesn't apply to the new version, so replace it,
+	// even if we're repalcing it with nothing.
+	version.ChangeLog = c.ChangeLog
+
+	var hashes []string
+	if metaHash != "" {
+		hashes = append(hashes, metaHash)
+	}
+
+	return append(hashes, versionHash), nil
 }
 
 // NewId is a helper to generate a new default length Id.
--- store/helpers.go
+++ store/helpers.go
@@ -1,12 +1,14 @@
 package store
 
 import (
+	"encoding/json"
 	"io"
 	"io/ioutil"
 
 	"github.com/leeola/errors"
 )
 
+// WriteReader writes the given reader's content to the store.
 func WriteReader(s Store, r io.Reader) (string, error) {
 	if s == nil {
 		return "", errors.New("Store is nil")
@@ -22,4 +24,39 @@ func WriteReader(s Store, r io.Reader) (string, error) {
 
 	h, err := s.Write(b)
 	return h, errors.Wrap(err, "store failed to write")
+}
+
+// MarshalAndWrite marshals the given interface to json and writes that to the store.
+func MarshalAndWrite(s Store, v interface{}) (string, error) {
+	b, err := json.Marshal(v)
+	if err != nil {
+		return "", errors.Stack(err)
+	}
+
+	h, err := s.Write(b)
+	if err != nil {
+		return "", errors.Stack(err)
+	}
+
+	return h, nil
+}
+
+// WriteBlob is a type safe version of MarshalAndWrite.
+func WriteBlob(s Store, v Blob) (string, error) {
+	return MarshalAndWrite(s, v)
+}
+
+// WriteMeta is a type safe version of MarshalAndWrite.
+func WriteMeta(s Store, v Meta) (string, error) {
+	return MarshalAndWrite(s, v)
+}
+
+// WriteMultiBlob is a type safe version of MarshalAndWrite.
+func WriteMultiBlob(s Store, v Meta) (string, error) {
+	return MarshalAndWrite(s, v)
+}
+
+// WriteVersion is a type safe version of MarshalAndWrite.
+func WriteVersion(s Store, v Version) (string, error) {
+	return MarshalAndWrite(s, v)
 }
