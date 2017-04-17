f11bf8713ea47545c9b2bb0230eafe5c8690867d
Lee Olayvar <leeolayvar@gmail.com>
2017-04-17 07:23:18 -0700

    feat: added small Options api poc

--- index/index.go
+++ index/index.go
@@ -2,6 +2,15 @@ package index
 
 import "github.com/leeola/kala/q"
 
+type Options map[string]interface{}
+
+// Index implements indexing and searching functionality for a kala store.
 type Index interface {
+	// Index the given data with Options describing how to index the data.
+	//
+	// Note that Options may or may not be required, it depends on the indexer
+	// and the desired features. For example, if FullTextSearch is desired
+	// one may specify `Options["fullTextSearch"] = "all"` and etc.
+	Index(data interface{}, opts Options) error
 	Search(q.Query) ([][]byte, error)
 }
