39bbe965f76d0beb0f5b3e6ec32d53476d0119c3
Lee Olayvar <leeolayvar@gmail.com>
2017-05-12 07:58:05 -0700

    feat: added basic write command
    Missing commit data, only writes json.

--- cmd/fixi/write.go
+++ cmd/fixi/write.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"strings"
 
+	"github.com/leeola/fixity"
+	"github.com/leeola/fixity/autoload"
+	"github.com/leeola/fixity/util/clijson"
 	"github.com/urfave/cli"
 )
 
 func WriteCmd(ctx *cli.Context) error {
+	if len(ctx.Args()) == 0 {
+		return cli.ShowCommandHelp(ctx, "write")
+	}
+
 	filePath := ctx.String("file")
 	if filePath != "" {
 		return errors.New("--file not implemented yet")
@@ -17,5 +27,27 @@ func WriteCmd(ctx *cli.Context) error {
 		return errors.New("--blob not implemented yet")
 	}
 
-	return errors.New("not implemented")
+	fixi, err := autoload.LoadFixity(ctx.GlobalString("config"))
+	if err != nil {
+		return err
+	}
+
+	b, err := clijson.CliJson(ctx.Args())
+	if err != nil {
+		return err
+	}
+
+	c := fixity.Commit{}
+	j := fixity.Json{
+		Json: json.RawMessage(b),
+	}
+
+	hashes, err := fixi.Write(c, j, nil)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(strings.Join(hashes, "\n"))
+
+	return nil
 }
