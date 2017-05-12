11710f9dafee07fe9f0b69136795b9abbd395d71
Lee Olayvar <leeolayvar@gmail.com>
2017-05-12 07:54:51 -0700

    feat: added experimental cli to json lib
     Probably break it off to it's own package in the future.. but for
    now, it lives here until it matures.

---
+++ util/clijson/clijson.go
@@ -0,0 +1,148 @@
+package clijson
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+	"strings"
+)
+
+// CliJson converts the given os.Args array into Json bytes.
+//
+//
+// WARNING: This package was sort of hacked together while experimenting
+// with the syntax. A proper implementation is needed.
+func CliJson(args []string) ([]byte, error) {
+	v := process(args)
+	if v == nil {
+		return nil, errors.New("empty args")
+	}
+
+	return json.Marshal(v)
+}
+
+func process(args []string) interface{} {
+	if len(args) == 0 {
+		return nil
+	}
+
+	var values []interface{}
+	for len(args) > 0 {
+		var v interface{}
+
+		switch {
+		case args[0] == "[":
+			v, args = list(args[1:])
+
+		case args[0] == "{":
+			v, args = object(args[1:])
+
+		case isKey(args[0]):
+			v, args = object(args)
+
+		default:
+			v, args = list(args[1:])
+		}
+
+		values = append(values, v)
+	}
+
+	// remove the implicit list if there is only one value.
+	if len(values) == 1 {
+		return values[0]
+	}
+
+	return values
+}
+
+func list(args []string) ([]interface{}, []string) {
+	// always return at least an empty list, never nil.
+	values := []interface{}{}
+
+	if len(args) == 0 {
+		return values, nil
+	}
+
+	for len(args) > 0 {
+		var v interface{}
+		switch {
+		case args[0] == "]":
+			return values, args[1:]
+
+		case args[0] == "[":
+			v, args = list(args[1:])
+
+		case args[0] == "{":
+			v, args = object(args[1:])
+
+		case isKey(args[0]):
+			v, args = object(args)
+
+		default:
+			v = value(args[0])
+			args = args[1:]
+		}
+
+		values = append(values, v)
+	}
+
+	return values, nil
+}
+
+func object(args []string) (map[string]interface{}, []string) {
+	// always return at least an empty map, never nil.
+	values := map[string]interface{}{}
+
+	if len(args) == 0 {
+		return values, nil
+	}
+
+	for len(args) > 0 {
+		var (
+			k string
+			v interface{}
+		)
+
+		if args[0] == "}" {
+			return values, args[1:]
+		}
+
+		// if it's not a key, end the creation of the object
+		if !isKey(args[0]) {
+			// don't crop the args
+			return values, args
+		}
+
+		kv := strings.SplitN(args[0], "=", 2)
+
+		k = kv[0]
+
+		switch kv[1] {
+		case "[":
+			v, args = list(args[1:])
+		case "{":
+			v, args = object(args[1:])
+		default:
+			v = value(kv[1])
+			args = args[1:]
+		}
+
+		values[k] = v
+	}
+
+	return values, nil
+}
+
+func value(s string) interface{} {
+	if v, err := strconv.Atoi(s); err == nil {
+		return int(v)
+	}
+	if v, err := strconv.ParseBool(s); err == nil {
+		return v
+	}
+	return s
+}
+
+func isKey(s string) bool {
+	return strings.Contains(s, "=")
+}
---
+++ util/clijson/clijson_test.go
@@ -0,0 +1,53 @@
+package clijson
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCliJson(t *testing.T) {
+	Convey("Scenario: parsing input", t, func() {
+		type test struct {
+			Input    []string
+			Expected string
+		}
+
+		tests := []test{
+			{
+				[]string{"foo=bar"},
+				`{"foo":"bar"}`,
+			},
+			{
+				[]string{"[", "foo=bar", "]"},
+				`[{"foo":"bar"}]`,
+			},
+			{
+				[]string{"{", "foo=bar", "baz=bat"},
+				`{"baz":"bat","foo":"bar"}`,
+			},
+			{
+				[]string{"{", "foo=bar", "baz=bat", "}"},
+				`{"baz":"bat","foo":"bar"}`,
+			},
+			{
+				[]string{"{", "foo=bar", "baz=[", "1", "str", "]", "}"},
+				`{"baz":[1,"str"],"foo":"bar"}`,
+			},
+		}
+
+		for _, test := range tests {
+			Convey(fmt.Sprintf("Given input: %s", strings.Join(test.Input, " ")), func() {
+				Convey("When run against CliJson", func() {
+					outB, err := CliJson(test.Input)
+					So(err, ShouldBeNil)
+					Convey("Then it should return the expected output", func() {
+						So(string(outB), ShouldEqual, test.Expected)
+					})
+				})
+			})
+		}
+	})
+}
