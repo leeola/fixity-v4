8badfd1f00cbb66b7a9e310b517bb20b15cb7fa1
Lee Olayvar <leeolayvar@gmail.com>
2017-05-14 12:17:41 -0700

    docs: added more index docstring

--- index.go
+++ index.go
@@ -2,13 +2,24 @@ package fixity
 
 import "github.com/leeola/fixity/q"
 
-type Field struct {
-	Field   string       `json:"field"`
-	Value   interface{}  `json:"value,omitempty"`
-	Options FieldOptions `json:"options,omitempty"`
-}
-
 // Index implements indexing and searching functionality for a fixity store.
+//
+// An index can be implemented by anything with basic querying abilities,
+// usually databases, to retrieve the associated key.
+//
+// Generally, an index must:
+//
+// 1. Translate the Fixity *Query into a format that makes sense in the
+//    indexer, such as translating *Query to a SQL statement as might be
+//    done by postgresql.
+// 2. Return hashes of the documents matching the given query.
+// 3. Paginate and limit the searches.
+// 4. Support searching all fields, if a query constraint has a field value
+//    of "*".
+//
+// It's acceptable that an indexer only implement a subset of these features,
+// as the user can choose which indexer to use. Eg, some indexers, especially
+// sql based indexers, may have difficulty searching all fields (point 4).
 type Index interface {
 	// Index the given hash and id to the given fields.
 	//
@@ -17,12 +28,24 @@ type Index interface {
 	// a search to query the latest version of each id.
 	Index(hash, id string, fields []Field) error
 
+	// Search the index for the given Query, returning the matching hashes or ids.
+	//
+	// Note that within the query, a field can be specified as `"*"` to signify
+	// all fields are to be checked. Some operators may not support "*", and
+	// behavior can differ between Index implementations.
 	Search(*q.Query) ([]string, error)
 
 	// TODO(leeola): Enable a close method to shutdown any
 	//
 	// // Close shuts down any connections that may need to be closed.
 	// Close() error
+}
+
+// Field of a document to be indexed via Index.Index.
+type Field struct {
+	Field   string       `json:"field"`
+	Value   interface{}  `json:"value,omitempty"`
+	Options FieldOptions `json:"options,omitempty"`
 }
 
 // Fields is a helper type for convenient mutation of a []Field.
