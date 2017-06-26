7152480bb585eba2b1940ed08ec44756e1a29270
Lee Olayvar <leeolayvar@gmail.com>
2017-06-26 06:04:21 -0700

    test: added test file (empty)

---
+++ fixities/local/blockchain_test.go
@@ -0,0 +1,18 @@
+package local
+
+import (
+	"testing"
+
+	"github.com/leeola/fixity"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func mustTestLocal() fixity.Fixity {
+}
+
+func TestBlockchainAppendContent(t *testing.T) {
+	f := mustTestLocal()
+
+	Convey("Scenario: AppendContent", t, func() {
+	})
+}
--- fixities/local/fixity_test.go
+++ fixities/local/fixity_test.go
@@ -1,3 +1,1 @@
 package local
-
-import . "github.com/smartystreets/goconvey/convey"
