a68b0b9f21a7857f1b818ee4fb1d224606ebbfa1
Lee Olayvar <leeolayvar@gmail.com>
2017-07-10 07:39:16 -0700

    docs: minor text tweaks

--- writerequest.go
+++ writerequest.go
@@ -55,13 +55,14 @@ type WriteRequest struct {
 	// Note that the algorithm is decided by the fixity.Store.
 	AverageChunkSize uint64 `json:"averageChunkSize,omitempty"`
 
-	// IgnoreDuplicateBlob will ignore this write if the blob exists.
+	// IgnoreDuplicateBlob will ignore this write if the blob exists for the id.
 	//
 	// An existing blob is determined to be one which *exists* on the chain,
 	// not specifically if it is the latest Content for it's id.
 	//
 	// This flag is used to ensure data exists on the blockchain, without
-	// causing any form of versioning contention.
+	// causing any form of versioning contention. Duplicate blobs are allowed
+	// for different Ids.
 	IgnoreDuplicateBlob bool
 
 	// Fields are the indexable fields that the data will be indexed with.
