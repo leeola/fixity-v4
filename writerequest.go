be049f219d2b65617c1c58eef26d435f5c893712
Lee Olayvar <leeolayvar@gmail.com>
2017-07-14 06:51:46 -0700

    feat: added chunk sizing tiers; 100KiB/1MiB/4MiB

--- writerequest.go
+++ writerequest.go
@@ -11,7 +11,7 @@ const (
 	// See WriteRequest.AverageChunkSize documentation for further explanation
 	// of chunk sizes.
 	//
-	// This value is 1024^2*4 bytes, 4MiB, chosen as a compromise for the
+	// This value is 1024^2*1 bytes, 1MiB, chosen as a compromise for the
 	// small and large files.
 	//
 	// Ideally it should strike a balance between too many chunks written while
@@ -20,20 +20,51 @@ const (
 	//
 	// If written data is commonly editing the first X bytes, this value should
 	// be overridden in the WriteRequest itself.
-	DefaultAverageChunkSize uint64 = 4194304
+	DefaultAverageChunkSize = medChunkSize
 
-	// autoChunkCount is the number of chunks SetChunkFromBytes/etc will set to.
+	// smallChunkSize is the size SetChunkSizeFromX will use for small data.
 	//
-	// Eg, if SetChunkFromBytes is given a byte array that is 1,048,576 bytes,
-	// then it will divide 1,048,576 by eg, 10 to set the rollSize to
-	// 104,857, effectively rolling the bytes into 10 parts when it's written.
-	autoChunkCount = 6
+	// This value is 1024*100 bytes, 100 KiB, chosen as a balance for very
+	// small files being able to be chunked but still being well over the
+	// hard limits of the Restic "average bits". At this time, that value is
+	// 14bits (12KiB). Reference for this value can be found here:
+	//
+	//    https://github.com/leeola/fixity/blob/master/chunkers/restic/restic.go#L12
+	//
+	// Note that that using this smallChunkSize is decided via the smallCutoff,
+	// below.
+	smallChunkSize uint64 = 102400
 
-	// 12KiB so that small files/data won't churn with a ton of chunks.
-	minAutoChunkSize = 12288
-	// 4MiB because chunks too large become hard to store/send/etc. Each
-	// chunk should be reasonbly fast to send between nodes.
-	maxAutoChunkSize = DefaultAverageChunkSize
+	// medChunkSize is the size SetChunkSizeFromX will use for medium data.
+	//
+	// This value is 1024^2 bytes, 1MiB, chosen as a balance for medium data.
+	//
+	// Note that using this medChunkSize is decided via the medCutoff, below.
+	medChunkSize uint64 = 1048576
+
+	// medChunkSize is the size SetChunkSizeFromX will use for large data.
+	//
+	// This value is 1024^2*4 bytes, 4MiB, chosen as a balance between total
+	// hashes created and size of chunks being sent over the wire for large
+	// data. Too big of a chunk would cause data being sent from one node
+	// to another to lag. This value should choose a middleground between
+	// total hashes and node transfer times.
+	//
+	// Note that largeChunkSize is chosen only if the total bytes are larger
+	// the medCutoff.
+	largeChunkSize uint64 = 4194304
+
+	// smallCutoff decides if the smallChunkSize is to be used or not.
+	//
+	// If the total bytes given to AssertChunkSize() is below this value,
+	// the smallChunkSize will be used.
+	smallCutoff = smallChunkSize * 10
+
+	// medCutoff decides if the medChunkSize is to be used or not.
+	//
+	// If the total bytes given to AssertChunkSize() is below this value,
+	// the medChunkSize will be used.
+	medCutoff = medChunkSize * 200
 )
 
 // WriteRequest represents a blob to be written alone with metadata.
@@ -88,20 +119,35 @@ func NewWrite(id string, rc io.ReadCloser, f ...Field) *WriteRequest {
 	}
 }
 
-func (req *WriteRequest) setChunkSize(averageChunkSize uint64) {
-	if averageChunkSize < minAutoChunkSize {
-		averageChunkSize = minAutoChunkSize
-	}
-	if averageChunkSize > maxAutoChunkSize {
-		averageChunkSize = maxAutoChunkSize
-	}
-	req.AverageChunkSize = averageChunkSize
-}
-
+// SetChunkSizeFromBytes sets the chunkSize from the given byte slice.
+//
+// The chunksize value is decided via AssertChunkSize().
 func (req *WriteRequest) SetChunkSizeFromBytes(b []byte) {
-	req.setChunkSize(uint64(len(b)) / autoChunkCount)
+	req.AverageChunkSize = AssertChunkSize(uint64(len(b)))
 }
 
+// SetChunkSizeFromFileInfo sets the chunkSize from the given FileInfo.
+//
+// The chunksize value is decided via AssertChunkSize().
 func (req *WriteRequest) SetChunkSizeFromFileInfo(fi os.FileInfo) {
-	req.setChunkSize(uint64(fi.Size()) / autoChunkCount)
+	req.AverageChunkSize = AssertChunkSize(uint64(fi.Size()))
+}
+
+// AssertChunkSize chooses a chunksize based on the byte size of the input.
+//
+// It uses a tiered approach, decided by the constant values smallCutoff and
+// medCutoff. In short:
+//
+// - If totalBytes is smaller than 1MiB, a chunkSize of 100KiB is used.
+// - If totalBytes is smaller than 200MiB, a chunkSize of 1MiB is used.
+// - If totalBytes is larger than 200MiB, a chunkSize of 4MiB is used.
+func AssertChunkSize(totalBytes uint64) (chunkSize uint64) {
+	switch {
+	case totalBytes < smallCutoff:
+		return smallChunkSize
+	case totalBytes < medCutoff:
+		return medChunkSize
+	default:
+		return largeChunkSize
+	}
 }
