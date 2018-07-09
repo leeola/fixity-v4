22894adb62743a6713681f8366216dac67202dd2
Lee Olayvar <leeola@fastmail.com>
2018-07-09 07:08:27 -0700

    test: update const check for update values

--- blob_test.go
+++ blob_test.go
@@ -1,34 +1,36 @@
 package fixity
 
-// func TestBlobTypeConsts(t *testing.T) {
-// 	testCases := []struct {
-// 		Value       BlobType
-// 		ExpectValue BlobType
-// 	}{
-// 		{
-// 			Value:       BlobTypeMutation,
-// 			ExpectValue: 1,
-// 		},
-// 		{
-// 			Value:       BlobTypeValues,
-// 			ExpectValue: 2,
-// 		},
-// 		{
-// 			Value:       BlobTypeData,
-// 			ExpectValue: 3,
-// 		},
-// 		{
-// 			Value:       BlobTypeParts,
-// 			ExpectValue: 4,
-// 		},
-// 		{
-// 			Value:       BlobTypeSchemaless,
-// 			ExpectValue: 5,
-// 		},
-// 	}
-// 	for _, testCase := range testCases {
-// 		if testCase.Value != testCase.ExpectValue {
-// 			t.Errorf("%s want:%d, got:%d", testCase.Value.String(), testCase.ExpectValue, testCase.Value)
-// 		}
-// 	}
-// }
+import "testing"
+
+func TestBlobTypeConsts(t *testing.T) {
+	testCases := []struct {
+		Value       BlobType
+		ExpectValue BlobType
+	}{
+		{
+			Value:       BlobTypeSchemaless,
+			ExpectValue: 0,
+		},
+		{
+			Value:       BlobTypeParts,
+			ExpectValue: 1,
+		},
+		{
+			Value:       BlobTypeData,
+			ExpectValue: 2,
+		},
+		{
+			Value:       BlobTypeValues,
+			ExpectValue: 3,
+		},
+		{
+			Value:       BlobTypeMutation,
+			ExpectValue: 4,
+		},
+	}
+	for _, testCase := range testCases {
+		if testCase.Value != testCase.ExpectValue {
+			t.Errorf("%s want:%d, got:%d", testCase.Value.String(), testCase.ExpectValue, testCase.Value)
+		}
+	}
+}
