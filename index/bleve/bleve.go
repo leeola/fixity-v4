8f31af8fbe6516392f0f9ceb3ce86adb4d1386d6
Lee Olayvar <leeola@fastmail.com>
2018-07-09 19:09:23 -0700

    feat: add keyword field analyzer for ref and id

--- index/bleve/bleve.go
+++ index/bleve/bleve.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 
 	"github.com/blevesearch/bleve"
+	"github.com/blevesearch/bleve/analysis/analyzer/keyword"
+	"github.com/blevesearch/bleve/mapping"
 )
 
 const (
@@ -25,12 +27,12 @@ func New(path string) (*Index, error) {
 
 	idIndex, err := newBleve(idPath)
 	if err != nil {
-		return nil, fmt.Errorf("newBleve:  %v", err)
+		return nil, fmt.Errorf("newBleve: %v", err)
 	}
 
 	refIndex, err := newBleve(refPath)
 	if err != nil {
-		return nil, fmt.Errorf("newBleve:  %v", err)
+		return nil, fmt.Errorf("newBleve: %v", err)
 	}
 
 	return &Index{
@@ -46,8 +48,7 @@ func newBleve(path string) (bleve.Index, error) {
 
 	index, err := bleve.Open(path)
 	if err == bleve.ErrorIndexMetaMissing {
-		mapping := bleve.NewIndexMapping()
-		index, err = bleve.New(path, mapping)
+		index, err = bleve.New(path, newMapping())
 		if err != nil {
 			return nil, fmt.Errorf("new ref index: %v", err)
 		}
@@ -57,4 +58,21 @@ func newBleve(path string) (bleve.Index, error) {
 		return nil, fmt.Errorf("open ref index: %v", err)
 	}
 	return index, nil
+}
+
+func newMapping() *mapping.IndexMappingImpl {
+	keywordFieldMapping := bleve.NewTextFieldMapping()
+	keywordFieldMapping.Analyzer = keyword.Name
+
+	indexMapping := bleve.NewIndexMapping()
+
+	// ids with non-alpha-num values were having trouble matching,
+	// such as "foo-bar". After searching, it appears a keyword
+	// analyzer is needed to allow the field to not be chopped up.
+	//
+	// ref: https://github.com/blevesearch/bleve/issues/844
+	indexMapping.DefaultMapping.AddFieldMappingsAt(fieldNameID, keywordFieldMapping)
+	indexMapping.DefaultMapping.AddFieldMappingsAt(fieldNameRef, keywordFieldMapping)
+
+	return indexMapping
 }
